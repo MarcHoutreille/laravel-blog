1c6a53375aaad573126d37de76060f26246e2718
MarcHoutreille <marc.houtreille@gmail.com>
2021-05-19 15:52:29 +0200

    Working on post system

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css" class="style">
+    <title>Jim's Blog</title>
+</head>
+<body>
+    <article>
+    <h1><a href="/posts/post-01">First Post</a></h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis in veritatis, earum at architecto esse nam non quibusdam quaerat a enim tempore animi expedita quasi doloribus error. Culpa, nisi suscipit?
+    Laborum nemo optio ab, dolores facilis hic culpa saepe provident magni eligendi? Cupiditate voluptate nostrum assumenda perferendis tempore architecto illum inventore. Modi nobis dolorum consequatur vero atque est illum hic!
+    Error, ex! Animi esse reprehenderit nemo culpa saepe neque obcaecati tenetur repudiandae sint ea atque natus a blanditiis libero eos dolore rem corrupti consequatur iure, earum quibusdam. Cum, vero illum?
+    Obcaecati vitae a similique nisi autem tenetur nobis saepe blanditiis repellendus nulla, exercitationem quisquam fuga aliquam aperiam iusto soluta neque tempora, id hic alias molestiae voluptas animi? Dolores, nulla mollitia.
+    Totam optio delectus voluptate corrupti laboriosam aliquid itaque ducimus nesciunt rerum aut? Sed consequatur reprehenderit accusamus aspernatur fugiat ducimus voluptas omnis accusantium velit et. Dolorem eius officiis iste aperiam cum.</p>
+    </article>
+    <article>
+    <h1><a href="/posts/post-02">Second Post</a></h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis in veritatis, earum at architecto esse nam non quibusdam quaerat a enim tempore animi expedita quasi doloribus error. Culpa, nisi suscipit?
+    Laborum nemo optio ab, dolores facilis hic culpa saepe provident magni eligendi? Cupiditate voluptate nostrum assumenda perferendis tempore architecto illum inventore. Modi nobis dolorum consequatur vero atque est illum hic!
+    Error, ex! Animi esse reprehenderit nemo culpa saepe neque obcaecati tenetur repudiandae sint ea atque natus a blanditiis libero eos dolore rem corrupti consequatur iure, earum quibusdam. Cum, vero illum?
+    Obcaecati vitae a similique nisi autem tenetur nobis saepe blanditiis repellendus nulla, exercitationem quisquam fuga aliquam aperiam iusto soluta neque tempora, id hic alias molestiae voluptas animi? Dolores, nulla mollitia.
+    Totam optio delectus voluptate corrupti laboriosam aliquid itaque ducimus nesciunt rerum aut? Sed consequatur reprehenderit accusamus aspernatur fugiat ducimus voluptas omnis accusantium velit et. Dolorem eius officiis iste aperiam cum.</p>
+    </article>
+    <article>
+    <h1><a href="/posts/post-03">Third Post</a></h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis in veritatis, earum at architecto esse nam non quibusdam quaerat a enim tempore animi expedita quasi doloribus error. Culpa, nisi suscipit?
+    Laborum nemo optio ab, dolores facilis hic culpa saepe provident magni eligendi? Cupiditate voluptate nostrum assumenda perferendis tempore architecto illum inventore. Modi nobis dolorum consequatur vero atque est illum hic!
+    Error, ex! Animi esse reprehenderit nemo culpa saepe neque obcaecati tenetur repudiandae sint ea atque natus a blanditiis libero eos dolore rem corrupti consequatur iure, earum quibusdam. Cum, vero illum?
+    Obcaecati vitae a similique nisi autem tenetur nobis saepe blanditiis repellendus nulla, exercitationem quisquam fuga aliquam aperiam iusto soluta neque tempora, id hic alias molestiae voluptas animi? Dolores, nulla mollitia.
+    Totam optio delectus voluptate corrupti laboriosam aliquid itaque ducimus nesciunt rerum aut? Sed consequatur reprehenderit accusamus aspernatur fugiat ducimus voluptas omnis accusantium velit et. Dolorem eius officiis iste aperiam cum.</p>
+    </article>
+</body>
+</html>
